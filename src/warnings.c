b7114d5fb32226ade8971df1c8bb929e88e017aa
seesee010 <sebastian.daubner2@gmail.com>
2026-06-03 22:26:56 +0200

    CHORE(warning.c): a new define - LO3_DEBUG to debug somethin

--- src/warnings.c
+++ src/warnings.c
@@ -3,34 +3,45 @@
 
 #include "./internal/core.h"
 
-void lo3_warn(const char *msg, const char *context) {
+#define LO3_DEBUG(msg, context) \	
+	if (_check_null(msg) == -1) { \
+		(void)fprintf(stderr, CLR_BOLD CLR_RED "[ERROR]: Something went wrong. \nOr msg == empty?\n" CLR_RESET); \						\
+	} \
+	_bare_send(msg, context, "DEBUG"); \
+
+static int _check_null(const char *msg) {
 
 	if (msg == NULL || msg[0] == '\0') {
-		lo3_error("Could not warn the shit out it!", context);
-		return;
+		
+		(void)fprintf(stderr, CLR_BOLD CLR_RED "[ERROR]: Something went wrong. \nOr msg == empty?\n" CLR_RESET);
+		return -1;
 	}
+	return 0;
+}
 
+static void _bare_send(const char *msg, const char *context, const char *type) {
+	
 	if (context != NULL) {
 		(void)fprintf(
-		    stderr, CLR_BOLD CLR_YELLOW "[WARNING]: %s " CLR_RESET "(?: '%s', line: %d)\n",
-		    msg, context, currentLine);
+		    stderr, CLR_BOLD CLR_RED "[%s]: %s " CLR_RESET "(?: '%s', line: %d)\n",
+		    type, msg, context, currentLine);
 	} else {
-		(void)fprintf(stderr, CLR_BOLD CLR_YELLOW "[WARNING]: %s \n" CLR_RESET, msg);
+		(void)fprintf(stderr, CLR_BOLD CLR_RED "[%s]: %s \n" CLR_RESET, type, msg);
 	}
+}
+
+void lo3_warn(const char *msg, const char *context) {
+
+	if (_check_null(msg) == -1) {
+		_bare_send("Could not send error message!", context, "ERROR");
+	}
+	_bare_send(msg, context, "WARN");
 }
 
 void lo3_error(const char *msg, const char *context) {
 
-	if (msg == NULL || msg[0] == '\0') {
-		lo3_warn("Could not error, because there was no error message for the error", NULL);
-		return;
+	if (_check_null(msg) == -1) {
+		_bare_send("Could not send error message!", context, "ERROR");
 	}
-
-	if (context != NULL) {
-		(void)fprintf(stderr,
-		              CLR_BOLD CLR_RED "[ERROR]: %s " CLR_RESET "(?: '%s', line: %d)\n",
-		              msg, context, currentLine);
-	} else {
-		(void)fprintf(stderr, CLR_BOLD CLR_RED "[ERROR]: %s \n" CLR_RESET, msg);
-	}
+	_bare_send(msg, context, "ERROR");
 }
